198fe6d548c3d0c3a2b2fd17e344701e69ff121b
Michal Vala <michal.vala@gmail.com>
2025-02-09 19:55:32 +0100

    beautiful

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,50 @@
 import { useEffect, useState } from 'preact/hooks'
 import './app.css'
 import Plot from 'react-plotly.js';
+import { format } from 'date-fns';
+
+interface Lap {
+  Frames: Frame[];
+  CarModel: Int16Array;
+  Track: Int16Array;
+  PlayerName: Int16Array;
+  PlayerNick: Int16Array;
+  PlayerSurname: Int16Array;
+  LapTimeMs: number;
+}
+
+interface Frame {
+  NormalizedCarPosition: number;
+  Gas: number;
+  Brake: number;
+  SteerAngle: number;
+  Gear: number;
+  RPM: number;
+  SpeedKmh: number;
+  CarCoordinates: number[];
+}
+
+function int16ArrayToString(bytes: Int16Array) {
+  let result = '';
+  for (let i = 0; i < bytes.length && bytes[i] != 0; i++) {
+    result += String.fromCharCode(bytes[i]);
+  }
+  return result;
+}
 
 export function App() {
-  const [frames, setFrames] = useState<any[]>([]);
+  const [lap, setLap] = useState<Lap | null>(null);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    const loadCoordinates = async () => {
+    const loadLap = async () => {
       try {
         const response = await fetch('1739096360.json'); // Replace with your file path
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
         const lap = await response.json()
-        setFrames(lap.Frames);
+        setLap(lap);
       } catch (err) {
         if (err instanceof Error) {
           setError(err.message);
@@ -24,154 +54,167 @@ export function App() {
         console.error("Error loading coordinates:", err);
       }
     };
-
-    loadCoordinates();
+    console.log("hellow")
+    loadLap();
   }, []); // Empty dependency array ensures this runs only once on mount
 
-  return (
-    <>
-      <div>
-      <Plot
-          data={[
-            {
-              x: frames.map(c => c.NormalizedCarPosition),
-              y: frames.map(c => c.Gas),
-              // type: 'scatter',
-              mode: 'lines',
-              name: "Throttle",
-              marker: { color: 'green' },
-            },
-            {
-              x: frames.map(c => c.NormalizedCarPosition),
-              y: frames.map(c => c.Brake),
-              // type: 'scatter',
-              mode: 'lines',
-              name: "Brake",
-              marker: { color: 'red' },
-            },
-          ]}
-          layout={{
-            width: 1400,
-            height: 100,
-            xaxis: { visible: false },
-            yaxis: { visible: false },
-            margin: {
-              l: 5, r: 5, t: 5, b: 5,
-            },
-            showlegend: false,
-          }}
-          config={{ staticPlot: true }}
-        />
-        <Plot
-          data={[
-            {
-              x: frames.map(c => c.NormalizedCarPosition),
-              y: frames.map(c => c.SteerAngle),
-              // type: 'scatter',
-              mode: 'lines',
-              name: "Throttle",
-              marker: { color: 'orange' },
-            },
-          ]}
-          layout={{
-            width: 1400,
-            height: 100,
-            xaxis: { visible: false },
-            yaxis: { visible: false },
-            margin: {
-              l: 5, r: 5, t: 5, b: 5,
-            }
-          }}
-          config={{ staticPlot: true }}
-        />
-        <Plot
-          data={[
-            {
-              x: frames.map(c => c.NormalizedCarPosition),
-              y: frames.map(c => c.Gear),
-              // type: 'scatter',
-              mode: 'lines',
-              name: "Throttle",
-              marker: { color: 'blue' },
-            },
-          ]}
-          layout={{
-            width: 1400,
-            height: 100,
-            xaxis: { visible: false },
-            yaxis: { visible: false },
-            margin: {
-              l: 5, r: 5, t: 5, b: 5,
-            }
-          }}
-          config={{ staticPlot: true }}
-        />
-        <Plot
-          data={[
-            {
-              x: frames.map(c => c.NormalizedCarPosition),
-              y: frames.map(c => c.RPM),
-              // type: 'scatter',
-              mode: 'lines',
-              name: "Throttle",
-              marker: { color: 'gray' },
-            },
-          ]}
-          layout={{
-            width: 1400,
-            height: 100,
-            xaxis: { visible: false },
-            yaxis: { visible: false },
-            margin: {
-              l: 5, r: 5, t: 5, b: 5,
-            }
-          }}
-          config={{ staticPlot: true }}
-        />
-        <Plot
-          data={[
-            {
-              x: frames.map(c => c.NormalizedCarPosition),
-              y: frames.map(c => c.SpeedKmh),
-              // type: 'scatter',
-              mode: 'lines',
-              name: "Throttle",
-              marker: { color: 'black' },
-            },
-          ]}
-          layout={{
-            width: 1400,
-            height: 100,
-            xaxis: { visible: false },
-            yaxis: { visible: false },
-            margin: {
-              l: 5, r: 5, t: 5, b: 5,
-            }
-          }}
-          config={{ staticPlot: true }}
-        />
-        <Plot
-          data={[
-            {
-              x: frames.map(c => -c.CarCoordinates[0]),
-              y: frames.map(c => c.CarCoordinates[2]),
-              // type: 'scatter',
-              mode: 'lines',
-              marker: { color: 'red' },
-            },
-          ]}
-          layout={{
-            width: 1400,
-            height: 800,
-            xaxis: { visible: false },
-            yaxis: { visible: false },
-            margin: {
-              l: 5, r: 5, t: 5, b: 5,
-            }
-          }}
-          config={{ staticPlot: true }}
-        />
-      </div>
+  if (lap != null) {
+    return (
+      <>
+        <h1>blabol</h1>
+        <ul>
+          <li>Track: {int16ArrayToString(lap.Track)}</li>
+          <li>Car: {int16ArrayToString(lap.CarModel)}</li>
+          <li>Time: {format(new Date(lap.LapTimeMs), 'mm:ss:SSS').toString()}</li>
+          <li>Name: {int16ArrayToString(lap.PlayerName)} {int16ArrayToString(lap.PlayerSurname)}</li>
+        </ul>
+        <div>
+          <Plot
+            data={[
+              {
+                x: lap.Frames.map(c => c.NormalizedCarPosition),
+                y: lap.Frames.map(c => c.Gas),
+                // type: 'scatter',
+                mode: 'lines',
+                name: "Throttle",
+                marker: { color: 'green' },
+              },
+              {
+                x: lap.Frames.map(c => c.NormalizedCarPosition),
+                y: lap.Frames.map(c => c.Brake),
+                // type: 'scatter',
+                mode: 'lines',
+                name: "Brake",
+                marker: { color: 'red' },
+              },
+            ]}
+            layout={{
+              width: 1400,
+              height: 100,
+              xaxis: { visible: false },
+              yaxis: { visible: false },
+              margin: {
+                l: 5, r: 5, t: 5, b: 5,
+              },
+              showlegend: false,
+            }}
+            config={{ staticPlot: true }}
+          />
+          <Plot
+            data={[
+              {
+                x: lap.Frames.map(c => c.NormalizedCarPosition),
+                y: lap.Frames.map(c => c.SteerAngle),
+                // type: 'scatter',
+                mode: 'lines',
+                name: "Throttle",
+                marker: { color: 'orange' },
+              },
+            ]}
+            layout={{
+              width: 1400,
+              height: 100,
+              xaxis: { visible: false },
+              yaxis: { visible: false },
+              margin: {
+                l: 5, r: 5, t: 5, b: 5,
+              }
+            }}
+            config={{ staticPlot: true }}
+          />
+          <Plot
+            data={[
+              {
+                x: lap.Frames.map(c => c.NormalizedCarPosition),
+                y: lap.Frames.map(c => c.Gear),
+                // type: 'scatter',
+                mode: 'lines',
+                name: "Throttle",
+                marker: { color: 'blue' },
+              },
+            ]}
+            layout={{
+              width: 1400,
+              height: 100,
+              xaxis: { visible: false },
+              yaxis: { visible: false },
+              margin: {
+                l: 5, r: 5, t: 5, b: 5,
+              }
+            }}
+            config={{ staticPlot: true }}
+          />
+          <Plot
+            data={[
+              {
+                x: lap.Frames.map(c => c.NormalizedCarPosition),
+                y: lap.Frames.map(c => c.RPM),
+                // type: 'scatter',
+                mode: 'lines',
+                name: "Throttle",
+                marker: { color: 'gray' },
+              },
+            ]}
+            layout={{
+              width: 1400,
+              height: 100,
+              xaxis: { visible: false },
+              yaxis: { visible: false },
+              margin: {
+                l: 5, r: 5, t: 5, b: 5,
+              }
+            }}
+            config={{ staticPlot: true }}
+          />
+          <Plot
+            data={[
+              {
+                x: lap.Frames.map(c => c.NormalizedCarPosition),
+                y: lap.Frames.map(c => c.SpeedKmh),
+                // type: 'scatter',
+                mode: 'lines',
+                name: "Throttle",
+                marker: { color: 'black' },
+              },
+            ]}
+            layout={{
+              width: 1400,
+              height: 100,
+              xaxis: { visible: false },
+              yaxis: { visible: false },
+              margin: {
+                l: 5, r: 5, t: 5, b: 5,
+              }
+            }}
+            config={{ staticPlot: true }}
+          />
+          <Plot
+            data={[
+              {
+                x: lap.Frames.map(c => -c.CarCoordinates[0]),
+                y: lap.Frames.map(c => c.CarCoordinates[2]),
+                // type: 'scatter',
+                mode: 'lines',
+                marker: { color: 'red' },
+              },
+            ]}
+            layout={{
+              width: 1400,
+              height: 800,
+              xaxis: { visible: false },
+              yaxis: { visible: false },
+              margin: {
+                l: 5, r: 5, t: 5, b: 5,
+              }
+            }}
+            config={{ staticPlot: true }}
+          />
+        </div>
+      </>
+    )
+  } else {
+    return <>
+      <h1>no data</h1>
     </>
-  )
+  }
 }
