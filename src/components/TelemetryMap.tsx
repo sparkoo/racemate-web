f0254689722837e76931045591769fae3e84be3d
Michal Vala <michal.vala@gmail.com>
2025-03-18 06:26:57 +0100

    keep raceline graph aspect ratio

--- src/components/TelemetryMap.tsx
+++ src/components/TelemetryMap.tsx
@@ -19,21 +19,24 @@ const TelemetryMap: FunctionalComponent<Props> = ({ lap, hoveredFrames }) => {
   const width = 800;
   const height = 800;
 
-  const xScale = d3
-    .scaleLinear()
-    .domain([
-      d3.min(lap.frames, (d) => d.car_coordinate_x) || 0,
-      d3.max(lap.frames, (d) => d.car_coordinate_x) || 1,
-    ])
-    .range([0, width]);
+  // TODO: cleanup. these are to keep the aspect ratio of the graph.
+  const xExtent = d3.extent(lap.frames, (d) => d.car_coordinate_x) as [
+    number,
+    number
+  ];
+  const yExtent = d3.extent(lap.frames, (d) => d.car_coordinate_z) as [
+    number,
+    number
+  ];
 
-  const yScale = d3
-    .scaleLinear()
-    .domain([
-      d3.min(lap.frames, (d) => d.car_coordinate_z) || 0,
-      d3.max(lap.frames, (d) => d.car_coordinate_z) || 1,
-    ])
-    .range([0, height]);
+  const maxExtent = Math.max(xExtent[1] - xExtent[0], yExtent[1] - yExtent[0]);
+  const centerX = (xExtent[0] + xExtent[1]) / 2;
+  const centerY = (yExtent[0] + yExtent[1]) / 2;
+
+  const domain = [centerX - maxExtent / 2, centerX + maxExtent / 2];
+
+  const xScale = d3.scaleLinear().domain(domain).range([0, width]);
+  const yScale = d3.scaleLinear().domain(domain).range([height, 0]);
 
   useEffect(() => {
     if (!svgRef.current) return;
@@ -45,7 +48,7 @@ const TelemetryMap: FunctionalComponent<Props> = ({ lap, hoveredFrames }) => {
       const line = d3
         .line<racemate.Frame>()
         .x((d) => xScale(d.car_coordinate_x))
-        .y((d) => yScale(d.car_coordinate_z))
+        .y((d) => yScale(-d.car_coordinate_z))
         .curve(d3.curveBasis); // Choose your curve type
 
       svg
@@ -54,7 +57,8 @@ const TelemetryMap: FunctionalComponent<Props> = ({ lap, hoveredFrames }) => {
         .attr("fill", "none")
         .attr("stroke", "steelblue")
         .attr("stroke-width", 1)
-        .attr("d", line);
+        .attr("d", line)
+        .attr("transform", `rotate(-50, ${width / 2}, ${height / 2})`);
 
       setCarDot(
         svg
@@ -66,18 +70,27 @@ const TelemetryMap: FunctionalComponent<Props> = ({ lap, hoveredFrames }) => {
           .attr("cy", 0)
           .attr("r", 5)
           .attr("display", null)
+          .attr("transform", `rotate(-50, ${width / 2}, ${height / 2})`)
       );
       setMapRendered(true);
     }
 
     carDot
       ?.attr("cx", xScale(lap.frames[hoveredFrames[0]].car_coordinate_x))
-      .attr("cy", yScale(lap.frames[hoveredFrames[0]].car_coordinate_z));
+      .attr("cy", yScale(-lap.frames[hoveredFrames[0]].car_coordinate_z));
   }, [lap, width, height, hoveredFrames]);
 
+  // TODO: somehow better set track image
   return (
     <div>
-      <svg ref={svgRef} width={width} height={height} />
+      <svg
+        ref={svgRef}
+        width={width}
+        height={height}
+        className={
+          "bg-[url('tracks/donington.svg')] w-full bg-cover bg-center bg-no-repeat"
+        }
+      />
     </div>
   );
 };
